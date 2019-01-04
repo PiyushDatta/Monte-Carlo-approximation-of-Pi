eb97d0f31651df0fb8b0cc9ba788a6f83740a496
PiyushDatta <piyushdattaca@gmail.com>
2019-01-03 21:30:12 -0500

    Made a script to populate our db. Currently it adds 1000 points, everytime you run it. In the future I'll add features to delete all the data in case we want to try a smaller size, and also implement args to our handler to take arguments for the number of points added, right now it is set to 1000. Will work on front end and rest api when I have more time.

---
+++ src/montecarlosim/api/management/commands/populate_db.py
@@ -0,0 +1,46 @@
+from random import random
+
+from django.core.management.base import BaseCommand
+
+from montecarlosim.api.models import Points
+
+
+class Command(BaseCommand):
+    # TODO: Implement after
+    args = '<foo bar ...>'
+    help = 'our help string comes here, implement after'
+
+    def _create_points(self, number_of_points=1000):
+        """
+        Creates number of Points objects in our db designated by number_of_points variable
+        :param number_of_points: integer
+        :return: None
+        """
+        for _ in range(number_of_points):
+            point = Points(x_point=generate_point(), y_point=generate_point())
+            point.save()
+
+    # TODO: Implement after
+    def _flush_all_points(self):
+        pass
+
+    def handle(self, *args, **options):
+        """
+        Used by our cli to handle the call/request to send to our db.
+        :param args: to be implemented
+        :param options: to be implemented
+        :return: None
+        """
+        self._create_points()
+
+
+def generate_point(length_of_field=1):
+    """
+    Generates a random float number.
+    :param length_of_field: int
+    :return: float
+    """
+    if isinstance(length_of_field, int):
+        return (.5 - random()) * length_of_field
+    else:
+        raise TypeError("length_of_field must be an integer")
--- src/montecarlosim/api/models.py
+++ src/montecarlosim/api/models.py
@@ -6,10 +6,3 @@ from django.db import models
 class Points(models.Model):
     x_point = models.FloatField()
     y_point = models.FloatField()
-
-
-def generate_x_point(length_of_field=1):
-    if isinstance(length_of_field, int):
-        return (.5 - random()) * length_of_field
-    else:
-        raise TypeError("length_of_field must be an integer")
